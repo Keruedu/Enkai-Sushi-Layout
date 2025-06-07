a994f01a6111ce7fdc25ea2799f48825750df659
Keru1712 <133781338+Keru1712@users.noreply.github.com>
2025-06-07 17:00:16 +0700

    update port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true, // Cho phép truy cập từ bên ngoài
+    open: true, // Tự động mở browser khi start
+  },
+  preview: {
+    port: 4173,
+    host: true,
+  }
 })
